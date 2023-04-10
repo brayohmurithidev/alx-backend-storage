e830001b295a4e81dff1de1f9c45f1c174e6d071
brayohmurithidev <murithibrianm@gmail.com>
2023-04-10 12:36:14 +0300

    user table updates

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- Creating a table user that has 3 columns
 --Col ID auto increments, not null and primary key. 
 --email unique and not null, name
-
-CREATE TABLE IF NOT EXISTS `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
